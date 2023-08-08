1c6a35df8df19d0591a846ab5807ea39482ca7db
reonjames <reon.james.7@gmail.com>
2023-08-08 19:19:02 +0530

    added components and completed the assignmnment

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { warningAlertComponent } from './WarningAlert/WarningAlert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    warningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule,
