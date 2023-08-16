814483f73615615866d01aa67e51ec1ec4b75ee7
reonjames <reon.james.7@gmail.com>
2023-08-16 23:06:10 +0530

    2nd assignment done

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -3,18 +3,27 @@ import { Component } from "@angular/core";
 
 @Component({
     selector:'app-server',
-    templateUrl:'./server.component.html'
+    templateUrl:'./server.component.html',
+    styleUrls:['./server.component.css']
 })
 export class ServerComponent{
     serviceId:number=12;
     serviceName:string='angular';
     clickToSee=false;
+    changeClick=false;
+    incr=0;
     addServer=['testserver1'];
+    incrArr=[];
     serverName='testserver';
     getClickToSee(){
         this.clickToSee=true;
     }
     onAddServer(){
         this.addServer.push(this.serverName);
-      }
+    }
+    onChangeClick(){
+        this.changeClick=!this.changeClick;
+        this.incr+=1;
+        this.incrArr.push(this.incr);
+    }
 }
