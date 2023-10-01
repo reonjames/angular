bf7ca97bc8af85897ad1df749d1777479bffc1b0
reonjames <reon.james.7@gmail.com>
2023-10-01 23:22:34 +0530

    recipe components interacted(talked)

--- app/recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ app/recipe-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,Output,EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,7 +7,12 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+@Output() recipeEmit=new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A test recipe','This is my first recipe','https://www.howtocook.recipes/wp-content/uploads/2021/05/Ratatouille-recipe.jpg')
   ];
+
+  onRecipeDet(recipe:Recipe){
+    this.recipeEmit.emit(recipe);
+  }
 }
